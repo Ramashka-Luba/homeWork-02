1a52d000914eeb2485a06bc169b94d810f29ce0b
Ramashka-Luba <lromashko911@gmail.com>
2026-08-07 00:07:59 +0300

    add hw-08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,25 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortedName = state
+            if(action.payload === 'up') {
+                return sortedName.sort((a,b) => a.name.localeCompare(b.name))
+            } else {
+                return sortedName.sort((a,b) => b.name.localeCompare(a.name))
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            const filteredName = state
+            filteredName.filter(s => s.age >= action.payload )
+                        .sort((a,b) => a.age - b.age)
+            return filteredName // need to fix
         }
         default:
             return state
     }
 }
+
+
